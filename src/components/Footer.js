fc01c5207bb298d6d332b601492498841fde883d
Marissa Douglass <mahdouglass@gmail.com>
2019-02-12 20:31:41 -0500

    Created basic footer with copyright information.

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = class extends React.Component {
+
+    render() {
+        return (
+            <footer class="footer">
+                <div class="content">
+                    <p>© Marissa Douglass 2019</p>
+                </div>
+            </footer>
+        )
+    }
+}
+
+export default Footer
