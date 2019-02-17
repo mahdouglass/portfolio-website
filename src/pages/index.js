5babd5eed19a4ae5de3200deb0a10546763a3891
Marissa Douglass <mahdouglass@gmail.com>
2019-02-16 20:15:26 -0500

    Removed recent posts from homepage and added sentence about me. Will probably revise in the future.

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,31 +13,8 @@ export default class IndexPage extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+              <h1 className="has-text-centered">Web developer based out of Springfield, OH.</h1>
             </div>
-            {posts
-              .map(({ node: post }) => (
-                <div
-                  className="content"
-                  style={{ border: '1px solid #fff', padding: '2em 4em' }}
-                  key={post.id}
-                >
-                  <h2>
-                    <Link to={post.fields.slug}>
-                      {post.frontmatter.title}
-                    </Link>
-                  </h2>
-                  <h4>{post.frontmatter.date}</h4>
-                  <p>
-                    {post.excerpt}
-                    <br />
-                    <br />
-                    <Link className="button is-small" to={post.fields.slug}>
-                      Keep Reading →
-                    </Link>
-                  </p>
-                </div>
-              ))}
           </div>
         </section>
       </Layout>
