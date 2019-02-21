a1711ca55eba9e02cd0bd148b81418dd0862be26
Marissa Douglass <mahdouglass@gmail.com>
2019-02-20 21:55:55 -0500

    Created another portfolio project piece for the cms so I can update through the site.

---
+++ src/cms/preview-templates/PortfolioProjectPreview.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { PortfolioProjectTemplate } from '../../templates/blog-post'
+
+const PortfolioProjectPreview = ({ entry, widgetFor }) => (
+  <PortfolioProjectTemplate
+    content={widgetFor('body')}
+    description={entry.getIn(['data', 'description'])}
+    tags={entry.getIn(['data', 'tags'])}
+    title={entry.getIn(['data', 'title'])}
+  />
+)
+
+PortfolioProjectPreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  widgetFor: PropTypes.func,
+}
+
+export default PortfolioProjectPreview
