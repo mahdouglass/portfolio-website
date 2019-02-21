d7158119f773807c29d0445dbe4b534636b08de8
Marissa Douglass <mahdouglass@gmail.com>
2019-02-20 22:02:21 -0500

    Missed updating path to look for portfolio project template.

--- src/cms/preview-templates/PortfolioProjectPreview.js
+++ src/cms/preview-templates/PortfolioProjectPreview.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { PortfolioProjectTemplate } from '../../templates/blog-post'
+import { PortfolioProjectTemplate } from '../../templates/portfolio-project'
 
 const PortfolioProjectPreview = ({ entry, widgetFor }) => (
   <PortfolioProjectTemplate
