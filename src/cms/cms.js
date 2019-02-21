8bc7a075f999a1b6cf188feef1d8b15ba149ce2e
Marissa Douglass <mahdouglass@gmail.com>
2019-02-20 22:03:49 -0500

    Imported portfolio project into cms.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,6 +2,8 @@ import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
+import PortfolioProjectPreview from './preview-templates/PortfolioProjectPreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('portfolio', PortfolioProjectPreview)
