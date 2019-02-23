8f4dff768c888389a3af084ff2fc1c37d3edd143
Marissa Douglass <mahdouglass@gmail.com>
2019-02-22 22:22:54 -0500

    Removed unused imports and constants from homepage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 
 export default class IndexPage extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
 
     return (
       <Layout>
